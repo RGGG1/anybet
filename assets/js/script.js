253a58f9c2843a9364e1220e1e5694630f83ff64
RGGG1 <>
2023-06-18 23:43:01 +0000

    working on the algo for betWin function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,32 @@
 function betWin()
 {
-var bet = document.getElementById("bet");
+const bet = document.getElementById("bet");
 console.log(bet.value)
-var win = document.getElementById("win");
+
+const win = document.getElementById("win");
 console.log(win.value)
 
-console.log(parseFloat(bet.value) + parseFloat(win.value))
+const oddsTotal = parseFloat(bet.value) + parseFloat(win.value);
+console.log(oddsTotal)
+
+const playerWinChance = ((parseFloat(bet.value) / oddsTotal)*100);
+console.log(playerWinChance)
+
+const playerNumber = (playerWinChance * oddsTotal);
+console.log(playerNumber)
+
+const min = 0;
+const max = oddsTotal;
+winningNumber = Math.round(Math.random() * (max - min) + (min));
+console.log(winningNumber);
+
+if (playerNumber <= winningNumber) {
+    outcome = "You Win";
+    
+
+  } else {
+    outcome = "You Lose";
+}
+
+console.log(outcome)
 }
