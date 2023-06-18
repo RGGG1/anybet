8165d42dd46e517653c590967719b374639d14fb
RGGG1 <>
2023-06-18 22:58:24 +0000

    creation of betWin function

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function betWin()
+{
+var bet = document.getElementById("bet");
+console.log(bet.value)
+var win = document.getElementById("win");
+console.log(win.value)
+
+console.log(parseFloat(bet.value) + parseFloat(win.value))
+}
